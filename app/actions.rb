6e8ef2eb52ee5606190b767fe93c69a3c1535ac0
pavel-piatetskii <pavel.piatetskii@gmail.com>
2020-09-16 03:06:38 +0000

    added Login/Logout and session_id

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,10 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
@@ -8,7 +15,6 @@ get '/signup' do     # if a user navigates to the path "/signup",
   @user = User.new   # setup empty @user object
   erb(:signup)       # render "app/views/signup.erb"
 end
-
 
 post '/signup' do
 
@@ -21,8 +27,34 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
-    "User #{username} saved!"
+    redirect to('/login')
   else
     erb(:signup)
   end
+end
+
+
+get '/login' do      # if a user navigates to the path "/signup",
+  erb(:login)       # render "app/views/signup.erb"
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
